c9e7e30da9a4795c9d09321a21412268a3712995
Roca Maya <rocamaya631@gmail.com>
2026-08-04 03:27:57 -0600

    fix: corregir importacion duplicada de app

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const app = require("./app");
-const dotenv = require("dotenv");
-
+const express = require('express'); 
+const app = require('./app');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}/auth/login`);
-});
 
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
